e66f3843d6c9523d427627a300ff33f311a8e837
reggeditt <tkodjosarso@gmail.com>
2025-01-31 13:23:36 +0000

    feat: implement dashboard layout with breadcrumb navigation and data context provider

--- components/dashboard/app-sider.js
+++ components/dashboard/app-sider.js
@@ -1,36 +1,16 @@
 "use client"
 
 import * as React from "react"
-import {
-  AudioWaveform,
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal,
-} from "lucide-react"
-
+import { AudioWaveform, BookOpen, Bot, Command, Frame, GalleryVerticalEnd, Map, PieChart, Settings2, SquareTerminal } from "lucide-react"
 import { NavMain } from "@/components/dashboard/nav-main"
-import { NavProjects } from "@/components/dashboard/nav-projects"
 import { NavUser } from "@/components/dashboard/nav-user"
 import { TeamSwitcher } from "@/components/dashboard/team-switcher"
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarRail,
-} from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail } from "@/components/ui/sidebar"
 
-// This is sample data.
 const data = {
   user: {
-    name: "shadcn",
-    email: "m@example.com",
+    name: "CanaanLand Faith Sanctuary",
+    email: "Cananlandfaithsanctuary@gmail.com",
     avatar: "/avatars/shadcn.jpg",
   },
   teams: [
@@ -52,106 +32,93 @@ const data = {
   ],
   navMain: [
     {
-      title: "Playground",
+      title: "Database",
       url: "#",
       icon: SquareTerminal,
       isActive: true,
       items: [
         {
-          title: "History",
-          url: "#",
+          title: "Members",
+          url: "/dashboard/members",
         },
         {
-          title: "Starred",
-          url: "#",
+          title: "Visitors",
+          url: "/dashboard/visitors",
         },
         {
-          title: "Settings",
-          url: "#",
+          title: "Children",
+          url: "/dashboard/children",
+        },
+        {
+          title: "Attendance",
+          url: "/dashboard/attendance",
         },
       ],
     },
     {
-      title: "Models",
+      title: "Communication",
       url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Announcements",
+          url: "/dashboard/announcements",
+        },
+        {
+          title: "Sms",
+          url: "/dashboard/sms",
+        },
+      ],
+    },
+    {
+      title: "Finance",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      items: [
+        {
+          title: "Income",
+          url: "/dashboard/income",
+        },
+        {
+          title: "Expenditure",
+          url: "/dashboard/expense",
+        },
+        {
+          title: "Accounts",
+          url: "/dashboard/accounts",
+        },
+        {
+          title: "Transactions",
+          url: "/dashboard/transactions",
+        },
+      ],
+    },
+    {
+      title: "Assets",
+      url: "/dashboard/assets",
       icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
+      items: [],
     },
     {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
+      title: "Admin",
+      url: "/dashboard/adminpanel",
       icon: Settings2,
       items: [
         {
-          title: "General",
-          url: "#",
+          title: "Events",
+          url: "dashboard/events",
         },
         {
-          title: "Team",
-          url: "#",
+          title: "Groups",
+          url: "dashboard/groups",
         },
         {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
+          title: "Users/Team",
+          url: "dashboard/users",
         },
       ],
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
     },
   ],
 }
@@ -164,7 +131,6 @@ export function AppSidebar({ ...props }) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
